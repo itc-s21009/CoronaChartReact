98d630e02bd72584c71a4639262031c22a91a241
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2021-12-01 06:14:17 +0000

    感染者数を日毎にまとめる関数

--- src/GenerateChart.jsx
+++ src/GenerateChart.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Area } from 'recharts'
 
+// 日ごとの感染者増加数をまとめる
+const createData = (data) => {
+    const data_new = []
+    for (let i = 1; i < data.length; i++) {
+        const day = data[i]
+        const day_previous = data[i - 1]
+        const patients = day.npatients - day_previous.npatients
+        data_new.push({ "date": day.date, "npatients": patients })
+    }
+    return data_new
+}
+
 const GenerateChart = ({ width, height, data }) => {
     return (
-        <AreaChart width={width} height={height} data={data}
+        <AreaChart width={width} height={height} data={createData(data)}
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
             <defs>
                 <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
