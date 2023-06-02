88a857b96ba2ab91f3a03d24f0cf517147e0bad7
itc-s21009 <s21009@std.it-college.ac.jp>
2023-06-02 12:17:58 +0900

    年選択の項目を今年まで出すようにした

--- src/YearSelectMenu.jsx
+++ src/YearSelectMenu.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import SelectMenu from './SelectMenu'
 
 const years = [
     "全期間",
-    "2020年",
-    "2021年"
 ]
 
 const YearSelectMenu = ({ selectedValue, onChange }) => {
+    const prepareYearItems = () => {
+        let start = 2020
+        const y = new Date().getFullYear()
+        while (start <= y) {
+            years.push(`${start++}年`)
+        }
+    }
+    useEffect(() => prepareYearItems(), [])
     return <SelectMenu selectedValue={selectedValue} values={years} onChange={onChange} />
 }
 
